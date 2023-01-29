9618a6f0ad4d29cb8ced55b49d891a1b76722fcf
jeremaquai <jeremaquai@gmail.com>
2023-01-29 03:50:50 -0600

    begginning layout

---
+++ src/features/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="pageTitle" >
+                <h1>
+                    Jeremaquai's Portfolio
+                </h1>
+            </div>
+        </header>
+    );
+}
